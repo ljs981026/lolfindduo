09bbfa2fbc5bd677298a6d084396bcb03a8e4ed5
ljs981026 <ljs981026@gmail.com>
2023-05-16 12:03:27 +0900

    lolfindduo v1.0.1

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './style/common.css';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <HashRouter basename='/'>
+        <App />
+    </HashRouter>
 );
 
 
