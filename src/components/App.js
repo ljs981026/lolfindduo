53acad63caa954281f3d3fcbf4cf21f4812d0051
ljs981026 <ljs981026@gmail.com>
2023-05-16 13:47:42 +0900

    lolfindduo v1.0.5

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ const App = () => {
             setTimeout(() => setInit(true), 2000);
         })
         return(
-            <BrowserRouter base="/">
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                     <Routes>
                         <Route path="/main" element={<Navigator isAuth={access} component={<Main />} />}></Route>
                         {/* <Route path="/main" element={<Main />}></Route>                                    */}
